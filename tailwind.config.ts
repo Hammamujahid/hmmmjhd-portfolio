e408a307a893ff30b8f5cd3354c4fdc49f2b05cb
Hammamujahid <hammamujahid@gmail.com>
2024-12-16 20:52:11 +0700

    First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",

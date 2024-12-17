1894143f60f86112809c11faa4b622a99b26d9fe
Hammamujahid <hammamujahid@gmail.com>
2024-12-17 23:19:43 +0700

    add control menu button

--- src/app/(pages)/dekstop/page.tsx
+++ src/app/(pages)/dekstop/page.tsx
@@ -12,6 +12,7 @@ const comfortaa = Comfortaa({
 const Dekstop = () => {
   const router = useRouter();
   const [name, setName] = useState("");
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
 
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
@@ -24,22 +25,56 @@ const Dekstop = () => {
   }, [router]);
 
   const handleOpenMenu = () => {
-    console.log("Menu opened!"); // Fungsi untuk handle menu
+    setIsOpenMenu((prevState) => !prevState);
   };
+
+  const handleBackLock = () => {
+    router.replace("./");
+  };
+
+  const handleClickOutside = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLElement;
+    if (!target.closest("#menu")) {
+      setIsOpenMenu(false);
+    }
+  };
+
   return (
     <div
+      onClick={handleClickOutside}
       className={`${comfortaa.className} w-screen h-screen bg-[url('/images/bg.jpeg')] bg-cover bg-bottom flex flex-col justify-between items-center`}
     >
-      <div className="w-full h-8 bg-blue-950/90 border-b-2 border-black flex justify-between items-center px-7">
-        <button
-          className="w-4 h-4 rounded-full border-black border-2 bg-white"
-          onClick={handleOpenMenu}
-        ></button>{" "}
-        <span>Welcome, {name}!</span>
+      <div className="flex flex-col w-full">
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="h-8 sm:bg-blue-950/90 bg-pink-400/90 border-b-2 border-black flex justify-between items-center px-7"
+        >
+          <button
+            className="w-4 h-4 rounded-full border-black border-2 bg-white"
+            onClick={handleOpenMenu}
+          ></button>
+          <span className="sm:text-base text-sm text-white">
+            Welcome, {name} !
+          </span>
+        </div>
+        {isOpenMenu == true ? (
+          <div
+            id="menu"
+            className="ml-4 w-1/3 sm:w-1/6 sm:bg-blue-950/70 bg-pink-400/70 rounded-b-md border-2 border-t-transparent border-black flex flex-col"
+          >
+            <button className="border-b border-white justify-start items-start flex px-2 pt-1 sm:hover:bg-blue-400 sm:text-base text-sm">
+              About Me
+            </button>
+            <button
+              className=" justify-start items-start flex px-2 pt-1 sm:hover:bg-blue-400 sm:text-base text-sm"
+              onClick={handleBackLock}
+            >
+              Lock Screen
+            </button>
+          </div>
+        ) : null}
       </div>
-      <div className="flex gap-2 px-4 h-10 bg-white/60">
-
-      </div>
+      <div className="flex gap-2 px-4 h-10 bg-white/30 sm:w-1/2 w-full sm:rounded-t-md"></div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,13 @@ const comfortaa = Comfortaa({
 export default function Home() {
   const [name, setName] = useState("");
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleNavigation = () => {
-    router.push(`/dekstop?name=${encodeURIComponent(name)}`);
+    setIsLoading(true);
+    setTimeout(() => {
+      router.push(`/dekstop?name=${encodeURIComponent(name)}`);
+    }, 4000);
   };
 
   return (
@@ -24,23 +28,32 @@ export default function Home() {
     >
       <div className="backdrop-blur-sm bg-white/10 flex flex-col items-center justify-center w-full h-full  gap-4">
         <div className="z-10 bg-[url('/images/photoprofile.jpg')] bg-cover w-20 h-20 bg-center rounded-full"></div>
-        <div className="text-sm font-bold z-10">Mujahid&apos;s Portfolio</div>
+        <div className="text-sm font-bold z-10 text-white">
+          Mujahid&apos;s Portfolio
+        </div>
         <div className="z-10 flex gap-2">
-          <input
-            type="text"
-            name="name"
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Enter your name"
-            className="rounded-md text-black bg-white/60 h-6 px-2 w-32 sm:w-40 focus:outline-none text-sm"
-          />
-          {name.trim() && (
-            <button
-              className="bg-transparent rounded-full"
-              onClick={handleNavigation}
-            >
-              <HiArrowRightCircle className="w-6 h-6 text-white/60" />
-            </button>
+          {!isLoading ? (
+            <>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                maxLength={15}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter your name"
+                className="rounded-md text-black bg-white/60 h-6 px-2 w-32 sm:w-40 focus:outline-none sm:text-sm text-xs"
+              />
+              {name.trim() && (
+                <button
+                  className="bg-transparent rounded-full"
+                  onClick={handleNavigation}
+                >
+                  <HiArrowRightCircle className="w-6 h-6 text-white/60 hover:text-white/70" />
+                </button>
+              )}{" "}
+            </>
+          ) : (
+            <div className="animate-spin z-10 w-5 h-5 border-4 border-white/60 border-t-transparent rounded-full"></div>
           )}
         </div>
       </div>

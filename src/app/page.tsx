71572527d830d1c302fb1ac014f1234069fea73f
Hammamujahid <hammamujahid@gmail.com>
2024-12-17 08:42:05 +0700

    cek tinggi layar hp 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,29 +20,29 @@ export default function Home() {
 
   return (
     <div
-      className={`${comfortaa.className} flex flex-col items-center justify-center w-screen min-h-screen bg-white gap-4 bg-[url('/images/bg.jpeg')] bg-cover bg-bottom`}
+      className={`${comfortaa.className} w-screen h-screen bg-[url('/images/bg.jpeg')] bg-cover bg-bottom`}
     >
-      <div className="absolute inset-0 backdrop-blur-sm bg-white/10"></div>
-
-      <div className="z-10 bg-[url('/images/photoprofile.jpg')] bg-cover w-20 h-20 bg-center rounded-full"></div>
-      <div className="text-sm font-bold z-10">Mujahid&apos;s Portfolio</div>
-      <div className="z-10 flex gap-2">
-        <input
-          type="text"
-          name="name"
-          id="name"
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Enter your name"
-          className="rounded-md text-black bg-white/60 h-6 px-2 w-32 sm:w-40 focus:outline-none text-sm"
-        />
-        {name.trim() && (
-          <button
-            className="bg-transparent rounded-full"
-            onClick={handleNavigation}
-          >
-            <HiArrowRightCircle className="w-6 h-6 text-white/60" />
-          </button>
-        )}
+      <div className="backdrop-blur-sm bg-white/10 flex flex-col items-center justify-center w-full h-full  gap-4">
+        <div className="z-10 bg-[url('/images/photoprofile.jpg')] bg-cover w-20 h-20 bg-center rounded-full"></div>
+        <div className="text-sm font-bold z-10">Mujahid&apos;s Portfolio</div>
+        <div className="z-10 flex gap-2">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your name"
+            className="rounded-md text-black bg-white/60 h-6 px-2 w-32 sm:w-40 focus:outline-none text-sm"
+          />
+          {name.trim() && (
+            <button
+              className="bg-transparent rounded-full"
+              onClick={handleNavigation}
+            >
+              <HiArrowRightCircle className="w-6 h-6 text-white/60" />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
